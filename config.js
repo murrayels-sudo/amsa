277950a2ecdc23428760632afe05edbc17dc852d
murrayels-sudo <murrayels@gmail.com>
2026-05-13 12:36:47 +1000

    Add files via upload

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 const CONFIG = {
 
   // Your Supabase project URL  (looks like https://xxxxxxxxxxxx.supabase.co)
-  SUPABASE_URL: 'https://YOUR_PROJECT_ID.supabase.co',
+  SUPABASE_URL: 'hhttps://github.com/murrayels-sudo/amsa
 
   // Your Supabase anon/public key  (long string starting with "eyJ…")
   SUPABASE_ANON_KEY: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqZ21wZ3lpcWZ0ZXF3dWRwY3FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MjkzMzcsImV4cCI6MjA5NDIwNTMzN30.BfSBSHIdj1dxS2oHjJv9zYUehNBMeCqFoClgPn4zRJc,
