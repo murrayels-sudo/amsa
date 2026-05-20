894bb29fbc1d119b4802618659a78eb04ffaabe1
murrayels-sudo <murrayels@gmail.com>
2026-05-20 14:44:12 +1000

    Update SUPABASE_ANON_KEY in config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 
 const CONFIG = {
   SUPABASE_URL: 'https://ijgmpgyiqfteqwudpcqf.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqZ21wZ3lpcWZ0ZXF3dWRwY3FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MjkzMzcsImV4cCI6MjA5NDIwNTMzN30.BfSBSHIdj1dxS2oHjJv9zYUehNBMeCqFoClgPn4zRJc',
   ADMIN_PASSWORD: 'MKEAdmin2024',
 };
 export default CONFIG;
