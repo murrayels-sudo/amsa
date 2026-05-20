4cf6e2b884550718ba0ec644bbc0b94d5a46033c
murrayels-sudo <murrayels@gmail.com>
2026-05-20 11:07:27 +1000

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@
 const CONFIG = {
 
   // Your Supabase project URL  (looks like https://axxxxxxxxxxxx.supabase.co)
-  SUPABASE_URL: 'https://ijgmpgyiqfteqwudpcqf.supabase.co'
+  SUPABASE_URL: 'https://ijgmpgyiqfteqwudpcqf.supabase.co',
  
   // Your Supabase anon/public key  (long string starting with "eyJ…")
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqZ21wZ3lpcWZ0ZXF3dWRwY3FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MjkzMzcsImV4cCI6MjA5NDIwNTMzN30.BfSBSHIdj1dxS2oHjJv9zYUehNBMeCqFoClgPn4zRJc,'
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqZ21wZ3lpcWZ0ZXF3dWRwY3FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MjkzMzcsImV4cCI6MjA5NDIwNTMzN30.BfSBSHIdj1dxS2oHjJv9zYUehNBMeCqFoClgPn4zRJc',
 
   ADMIN_PASSWORD:   'MKEAdmin2024',
   
